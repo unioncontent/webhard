685abd4c69e4b7905ff74fb8a1af1e208bdbd0fc
BYUN SOOYEON <smb1457@naver.com>
2018-02-13 19:03:43 +0900

    필터링현황 개발중 (filetering 39번줄부터 수정-search넣기)

--- autogreen/routes/filtering.js
+++ autogreen/routes/filtering.js
@@ -1,15 +1,65 @@
 var express = require('express');
-var mysql      = require('mysql');
-var dbconfig   = require('../db/db_con.js');
-var connection = mysql.createConnection(dbconfig);
+var Filtering = require('../models/filtering.js');
+var User = require('../models/user.js');
+var moment = require('moment');
 var router = express.Router();
+
+//Difine variable for Pagination
+var totalUser = 0;
+var pageSize = 10;
+var pageCount = 0;
+var start = 0;
+var currentPage = 1;
+var cpId = '0';
 
 /* GET page. */
 router.get('/', function(req, res, next) {
-  if(!req.user){
-    res.redirect('/login');
+  // if(!req.user){
+  //   res.redirect('/login');
+  // }
+  if (typeof req.query.cpId !== 'undefined') {
+    cpId = req.query.cpId;
   }
-  res.render('filtering')
+
+  Filtering.filteringCount(cpId,function(err,result) {
+    if(err) throw err;
+    totalUser = result[0].total;
+    pageCount = Math.ceil(totalUser/pageSize);
+
+    if (typeof req.query.page !== 'undefined') {
+      currentPage = req.query.page;
+    }
+    else{
+      currentPage = 1
+    }
+
+    if (parseInt(currentPage)>0) {
+      start = (currentPage - 1) * pageSize;
+    }
+    Filtering.getFilteringList({cpId: cpId, offset: start, limit: pageSize}, function(err,result){
+      if(err){
+        res.json(err);
+      }else{
+        res.render('filtering',{
+          moment: moment,
+          cpId: cpId,
+          fList: result,
+          totalUser: totalUser,
+          minusCount: (currentPage - 1) * pageSize,
+          pageCount: pageCount,
+          pageSize: pageSize,
+          currentPage: currentPage
+        });
+      }
+    });
+  });
+});
+
+router.post('/getCPList', function(req, res, next){
+  User.getClassAllList('c',function(err,result){
+    if(err) throw err;
+    res.send(result);
+  });
 });
 
 module.exports = router;
--- autogreen/routes/manual.js
+++ autogreen/routes/manual.js
@@ -1,7 +1,5 @@
 var express = require('express');
-var mysql      = require('mysql');
-var dbconfig   = require('../db/db_con.js');
-var connection = mysql.createConnection(dbconfig);
+// var Manual = require('../models/manual.js');
 var router = express.Router();
 
 /* GET page. */
@@ -9,11 +7,7 @@ router.get('/', function(req, res, next) {
   if(!req.user){
     res.redirect('/login');
   }
-  var allList = [];
-  connection.query('SELECT * from fileis_cnts_all_a', function(err, rows, fields) {
-    if(err) throw err;
-    allList = rows;
-  });
+
 
   res.render('manual',{list: allList})
 });
