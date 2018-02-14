f1bab0d1c088ef37409b58d51d39a59e3e848037
BYUN SOOYEON <smb1457@naver.com>
2018-02-14 13:02:27 +0900

    models sql 수정 중(필터링현황테스트 해보기!)

--- autogreen/models/filtering.js
+++ autogreen/models/filtering.js
@@ -21,15 +21,25 @@ var Filtering = {
     var param = Object.values(item);
     connection.query(sql+' order by csState desc limit ?,?',param,callback);
   },
-  filteringCount : function(cpId,callback) {
+  filteringCount : function(item,callback) {
+    delete item.offset;
+    delete item.limit;
     var sql = 'select count(1) as total from fileis_filtering where cp_id=?';
-    if(cpId == '0'){
-      sql = 'select count(1) as total from fileis_filtering';
-      connection.query(sql, callback);
+    if(item.cpId == '0'){
+      sql = 'select count(1) from fileis_filtering where search is not null';
+      delete item.cpId;
     }
-    else{
-      connection.query(sql, cpId, callback);
+    if('searchType' in item){
+      switch (item.searchType) {
+        case 'i': sql+=' and osp_idx=\''+item.search+'\''; break;
+        case 'c': sql+=' and cnt_id=\''+item.search+'\''; break;
+        case 't': sql+=' and search like \'%'+item.search+'%\''; break;
+      }
+      delete item.searchType;
+      delete item.search;
     }
+    var param = Object.values(item);
+    connection.query(sql, param, callback);
   }
 }
 
