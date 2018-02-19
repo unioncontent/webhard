3b18028f91e9ada89f425b3919d3d0664bfde271
BYUN SOOYEON <smb1457@naver.com>
2018-02-19 17:07:17 +0900

    콘텐츠리스트,등록 페이지 완료 (콘텐츠등록 엑셀기능 추후 개발예정)

--- autogreen/routes/contents.js
+++ autogreen/routes/contents.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var Keyword = require('../models/keyword.js');
 var Contents = require('../models/contents.js');
 var User = require('../models/user.js');
 var router = express.Router();
@@ -63,22 +64,82 @@ router.get('/', function(req, res, next) {
       }
     });
   });
-  // res.render('contents');
 });
 
 router.post('/getCPList', function(req, res, next){
+  if(!req.user){
+    res.redirect('/login');
+  }
   User.getClassAllList('c',function(err,result){
     if(err) throw err;
     res.send(result);
   });
 });
 
-
-router.get('/add', function(req, res, next) {
+router.post('/update', function(req, res, next){
   if(!req.user){
     res.redirect('/login');
   }
+  Keyword.updateKeyword(req.body,function(err,result){
+    if(err) throw err;
+    res.send('true');
+  });
+});
+
+router.post('/delete', function(req, res, next){
+  if(!req.user){
+    res.redirect('/login');
+  }
+  Keyword.deleteKeyword(req.body.n_idx_c, function(err,result){
+    if(err) throw err;
+    Content.deleteContents(req.body.n_idx_c, function(err,result){
+      if(err) throw err;
+      res.send('true');
+    });
+  });
+});
+
+router.get('/add', function(req, res, next) {
+  // if(!req.user){
+  //   res.redirect('/login');
+  // }
   res.render('contentsAdd')
+});
+
+router.post('/add', function(req, res, next) {
+  // if(!req.user){
+  //   res.redirect('/login');
+  // }
+  var now = new Date();
+  var date = now.getFullYear() + '-' + (now.getMonth()+1) + '-' + now.getDate() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+  req.body.date = date;
+  User.searchOSP(req.body.OSP_id,function(err, results, fields) {
+    if(err) throw err;
+    // 콘텐츠 ID 설정
+    var check = false;
+    var sql = 'select count(1) as total from fileis_cnts_list_c';
+    Contents.contentsCount(req.body,function(err, results, fields) {
+      req.body.CP_CntID = req.body.U_id_c+'-'+req.body.OSP_id+'-'+String(Number(results[0].total)+1);
+      check = Contents.insertContents(req.body, function(err, results, fields) {
+        if(err) throw err;
+        var kParam = req.body;
+        if(results[0].n_idx != ""){
+          kParam.n_idx_c = results[0].n_idx;
+          console.log('kParam : ',kParam);
+          Keyword.insertKeyword(kParam,function(err, results, fields) {
+            if(err) throw err;
+            check = true;
+            if(check){
+              res.send('콘텐츠 등록이 완료되었습니다.');
+            }
+          });
+        }
+      });
+      if(check == false){
+        res.status(500).send('다시 입력해 주세요.');
+      }
+    });
+  });
 });
 
 router.get('/keyword', function(req, res, next) {
