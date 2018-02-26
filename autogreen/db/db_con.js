ebb2b2f7063d4f33ca86eb4d102eb881df67d069
BYUN SOOYEON <smb1457@naver.com>
2018-02-26 17:36:33 +0900

    db 정보 수정

--- autogreen/db/db_con.js
+++ autogreen/db/db_con.js
@@ -1,7 +1,14 @@
+// module.exports = {
+//   host     : '192.168.0.16',
+//   user     : 'soas',
+//   password : 'qwer1234',
+//   port     : '3306',
+//   database : 'webhard'
+// };
 module.exports = {
-  host     : '192.168.0.16',
-  user     : 'soas',
-  password : 'qwer1234',
+  host     : '180.210.20.178',
+  user     : 'autogreen',
+  password : 'uni1004',
   port     : '3306',
-  database : 'webhard'
+  database : 'fileis'
 };
