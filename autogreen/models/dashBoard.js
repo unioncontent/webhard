ff7ee27236fc95dc868dc331f5e0d8ecf506071f
BYUN SOOYEON <smb1457@naver.com>
2018-03-08 14:41:38 +0900

    대시보드 그래프 수정

--- autogreen/models/dashBoard.js
+++ autogreen/models/dashBoard.js
@@ -29,27 +29,27 @@ var DashBoard = {
     });
   },
   get24DataList: function(callback){
-    var sql = "SELECT FORMAT(COUNT(IF(K_apply='T' and CS_state='1',1,null)),0) as tTotal,\
-    FORMAT(COUNT(IF(K_apply='D' and CS_state='1',1,null)),0) as dTotal,\
-    FORMAT(COUNT(IF(K_apply='P',1,null)),0) as pTotal,\
-    DATE_FORMAT(CS_regdate,'%Y-%m-%d %H:00:00') as date\
-    FROM dashboard where CS_regdate > DATE_ADD(now(), INTERVAL -24 HOUR)\
-    group by hour(CS_regdate);";
-
+    // var sql = "SELECT FORMAT(COUNT(IF(K_apply='T' and CS_state='1',1,null)),0) as tTotal,\
+    // FORMAT(COUNT(IF(K_apply='D' and CS_state='1',1,null)),0) as dTotal,\
+    // FORMAT(COUNT(IF(K_apply='P',1,null)),0) as pTotal,\
+    // DATE_FORMAT(CS_regdate,'%Y-%m-%d %H:00:00') as date\
+    // FROM dashboard where CS_regdate > DATE_ADD(now(), INTERVAL -24 HOUR)\
+    // group by hour(CS_regdate);";
+    var sql = "call 24Data();"
     // console.log(sql);
     // var connection = mysql.createConnection(info.changeDB(global.osp));
     // connection.query(sql,callback);
     var DBpromise = new promise(global.osp);
     DBpromise.query(sql)
     .then(rows => {
-      return callback(rows);
+      // console.log(rows);
+      return callback(null,rows);
     })
     .then(rows => {
       DBpromise.close();
     })
     .catch(function (err) {
-      DBpromise.close();
-      console.log(err);
+      return callback(err,null);
     });
   }
 }
