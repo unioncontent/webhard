30e13091d2adfd48023ccb3acf809edf03cf20b6
BYUN SOOYEON <smb1457@naver.com>
2018-08-27 13:22:05 +0900

    오토그린 삭제복원 오류 수정

--- autogreen/models/osp/apiInfo.js
+++ autogreen/models/osp/apiInfo.js
@@ -7,5 +7,23 @@ module.exports = {
         'idx': idx
       };
     }
-  } 
+  },
+  sharebox : {
+    url:'http://dev.sharebox.co.kr/append/autoGreen/proc.php',
+    deletePost: function(mode,idx){
+      return {
+        'mode': mode,
+        'idx': idx
+      };
+    }
+  },
+  me2disk : {
+    url:'http://me2disk.com/append/autoGreen/proc.php',
+    deletePost: function(mode,idx){
+      return {
+        'mode': mode,
+        'idx': idx
+      };
+    }
+  }
 }
