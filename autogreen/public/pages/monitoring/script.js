3391651543e9bed44b8caab053cd54b0f3da543a
BYUN SOOYEON <smb1457@naver.com>
2018-09-12 16:21:53 +0900

    비제휴사현황,제휴사현황 완료

---
+++ autogreen/public/pages/monitoring/script.js
@@ -0,0 +1,427 @@
+$(document).ready(function(){
+  startSetting();
+
+  var z_result = $.urlParam('z-result');
+  if(z_result == 'false'){
+    swal("ERROR!", "다운로드에 실패했습니다!", "error");
+    settingParams(1);
+  }
+
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true
+  });
+});
+var modalInputEle = {
+  cnt_cp:$("#cnt_cp"),
+  cnt_mcp:$("#cnt_mcp"),
+  osp_sname:$("#osp_sname"),
+  title:$("#title"),
+  cnt_num:$("#cnt_num"),
+  cnt_price:$("#cnt_price"),
+  cnt_writer:$("#cnt_writer"),
+  k_title:$("#k_title"),
+  cnt_title:$("#cnt_title"),
+  cnt_url:$("#cnt_url"),
+  cnt_regdate:$("#cnt_regdate"),
+  go_regdate:$("#go_regdate"),
+  cnt_img_name:$("#cnt_img_name")
+};
+// 엑셀 클릭시
+$(document).on('click','.btn-excel',function(){
+  swal({
+    title: "엑셀출력하시겠습니까?",
+    icon: "warning",
+    buttons: ["취소", true],
+    dangerMode: true
+  })
+  .then(function(value) {
+    if (value != null) {
+      var param = settingParams('excel');
+      location.href = window.location.pathname+'/excel'+encodeQueryString(param);
+    }
+  });
+});
+
+// 이미지 클릭시
+$(document).on('click','.btn-image',function(){
+  swal({
+    title: "이미지 받으시겠습니까?",
+    icon: "warning",
+    buttons: ["취소", true],
+    dangerMode: true
+  })
+  .then(function(value) {
+    if (value != null) {
+      var param = settingParams('image');
+      location.href = window.location.pathname+'/image'+encodeQueryString(param);
+    }
+  });
+});
+
+function encodeQueryString(params) {
+    const keys = Object.keys(params)
+    return keys.length
+        ? "?" + keys
+            .map(key => encodeURIComponent(key)
+                + "=" + encodeURIComponent(params[key]))
+            .join("&")
+        : ""
+}
+
+// 제목 클릭시
+$(document).on('click','.info',function(){
+  var idx = $(this).data('idx');
+  // modal 초기화
+  $('#info-Modal p').text('');
+  $.ajax({
+    url: '/monitoring/getInfo',
+    type: 'post',
+    data: {idx:idx},
+    error: function(request,status,error){
+      errorMSG();
+    },
+    success: function(data){
+      for (var key in modalInputEle) {
+        if(key == 'go_regdate'){
+          continue;
+        }
+        modalInputEle[key].text(data[key]);
+      }
+      var dateArr = data['go_regdate'].split(',');
+      var imgArr = data['cnt_img_name'].split(',');
+
+      $('#img-list').empty();
+      for (var i = 1; i < 4; i++) {
+        var dateStr = (dateArr[i-1] == undefined) ? '':dateArr[i-1];
+        var imgHtml = '<div class="col-md-4"><div class="articles">\
+        <img src="/monitoring_img'+data['cnt_img_'+i]+'" class="img-fluid c-pointer" alt="">\
+        <h6>'+dateStr+'</h6></div></div>';
+        $('#img-list').append(imgHtml);
+      }
+      // $.each(dateArr, function( idx, date ) {
+      //   var path_f = '/monitoring_img';
+      //   if(imgArr[idx].indexOf('untitled') == -1){
+      //     path_f += data['path'];
+      //   }
+      //   var imgHtml = '<div class="col-md-4"><div class="articles">\
+      //   <img src="'+path_f+'/'+imgArr[idx]+'" class="img-fluid c-pointer" alt="">\
+      //   <h6>'+date+'</h6></div></div>';
+      //   $('#img-list').append(imgHtml);
+      // });
+      $('#info-Modal').modal('show');
+    }
+  });
+});
+
+// cp선택시
+$(document).on('change','#selectCP',function(){
+  var param = settingParams(1);
+  ajaxGetPageList(param);
+});
+// mcp선택시
+$(document).on('change','#selectMCP',function(){
+  $("#selectCP").empty();
+  $("#selectCP").append('<option value="">CP사선택</option>');
+
+  var mcpValue = $("#selectMCP option:selected").val();
+  var param = {};
+  if(mcpValue != ''){
+    param.mcp = mcpValue;
+  }
+  $.ajax({
+    url: '/kwd/getCP',
+    type: 'post',
+    data : param,
+    datatype : 'json',
+    error:function(request,status,error){
+      console.log('code:'+request.status+'\n'+'message:'+request.responseText+'\n'+'error:'+error);
+      alert('cp리스트 불러올 수 없습니다. 다시 시도해주세요.');
+    },
+    success:function(data){
+      if(data.status != true){
+        alert('cp리스트 불러올 수 없습니다. 다시 시도해주세요.');
+        return false;
+      }
+      data.result.forEach(function(item,idx){
+        var html = '<option value="'+item.cp_id+'">'+item.cp_cname+'</option>';
+        $("#selectCP").append(html);
+      });
+    }
+  });
+
+  param = settingParams(1);
+  ajaxGetPageList(param);
+});
+//게시물 삭제
+$(document).on('click','.btn-delete',function(){
+  var idx = $(this).data('idx');
+  var url = $(this).data('url');
+  swal({
+    title: "게시물 삭제 하시겠습니까?",
+    icon: "warning",
+    buttons: ["취소", true],
+    dangerMode: true
+  })
+  .then((value) =>{
+    if(value != null){
+      $.ajax({
+        url: '/monitoring/delete',
+        type: 'post',
+        data: {
+          'idx': idx,
+          'url': url
+        },
+        error:function(request,status,error){
+          errorMSG();
+        },
+        success:function(data){
+          if(data){
+            alert('삭제 되었습니다.');
+            reloadPage();
+          }
+        }
+      });
+    }
+  });
+});
+//날짜 선택 시
+$(document).on('click','.applyBtn',searchFun);
+// 검색
+$('#searchInput').on('keyup',function(event){
+  if(event.keyCode == 13){
+    $('#searchBtn').trigger('click');
+  };
+});
+$('#searchBtn').on('click',searchFun);
+
+//페이지 이동
+$(document).on('click','.page-link',function(){
+  var param = settingParams(Number($(this).data().value));
+  ajaxGetPageList(param);
+});
+// 리스트 새로고침
+function reloadPage(){
+  console.log('reloadPage');
+  var pageValue = ($.urlParam("page") == '' || $.urlParam("page") == null) ? 1 : parseInt($.urlParam("page"));
+  var param = settingParams(pageValue);
+  ajaxGetPageList(param);
+}
+// 페이지 ajax
+function ajaxGetPageList(param){
+  console.log('ajaxGetPageList:',param);
+  var pType = $('#pType').val();
+  $('.preloader3').show();
+  $.ajax({
+    url: '/monitoring/'+((pType == 'nalliance')?'비제휴사현황':'제휴사현황')+'/getNextPage',
+    type: 'post',
+    data: param,
+    error: function(request,status,error){
+      errorMSG();
+    },
+    success: function(data){
+      console.log(data);
+      if(data.status != true){
+        errorMSG();
+        return false;
+      }
+      $('#listTable tbody').empty();
+      data.result.list.forEach(function(item,idx){
+        var numIdx = Math.ceil(data.result.listCount-idx-(data.result.page-1)).toString();
+        var html = '<tr><th>'+numIdx+'</th>\
+          <td>'+item.osp_sname+'</td>\
+          <td>'+item.cnt_mcp+"/"+item.cnt_cp+'</td>\
+          <td>'+item.cnt_title+'</td>\
+          <td>'+item.k_title+'</td>\
+          <td><div class="info" data-idx="'+item.n_idx+'">'+item.title+'</div></td>\
+          <td>'+((item.cnt_price == null) ? '' :item.cnt_price)+'</td>\
+          <td>'+((item.cnt_writer == null) ? '' :item.cnt_writer)+'</td>';
+        html +='<td><div class="dateArr">';
+        var arr = item.go_regdate.split(',');
+        for(var j=0; j < arr.length; j++){
+          html +=(j+1)+' 차 : '+arr[j];
+          if (j < (arr.length-1)){
+            html += '<br />';
+          }
+        }
+        html +='</div></td><td><button type="button" class="btn btn-primary icon-btn waves-effect waves-light text-center"><i class="fas fa-download"></i></button></td>\
+          <td>채증</td>\
+          <td><button type="button" data-idx="'+item.n_idx+'" data-url="'+item.url+'" class="btn-delete btn btn-danger icon-btn waves-effect waves-light text-center"><i class="far fa-trash-alt"></i></button></td>\
+        </tr>';
+        $('#listTable tbody').append(html);
+      });
+      var limit = 20;
+      var pageCount = Math.ceil(data.result.listCount/limit);
+      $('#listTable tfoot').empty();
+      if(pageCount > 1) {
+        var html = '<tr><td colspan="12"><ul class="pagination float-right">';
+        var pageSize = 5;
+        var currentPage = data.result.page;
+        var pRCnt = parseInt(currentPage / pageSize);
+        if(currentPage % pageSize == 0){
+          pRCnt = parseInt(currentPage / pageSize) - 1;
+        }
+        if(currentPage > 5) {
+          html += '<li class="page-item"><a class="page-link" data-value="1" aria-label="Previous">\
+              <i class="ti-angle-double-left f-12"></i>\
+              <span aria-hidden="true"></span>\
+              <span class="sr-only">Previous</span>\
+            </a></li>\
+            <li class="page-item"><a class="page-link" data-value=\"'+(pRCnt * pageSize)+'"\ aria-label="Previous">\
+              <i class="ti-angle-left f-12"></i>\
+              <span aria-hidden="true"></span>\
+              <span class="sr-only">Previous</span>\
+            </a></li>';
+        }
+
+        for(var index=pRCnt * pageSize + 1;index<(pRCnt + 1)*pageSize + 1;index++){
+          var active = (currentPage == index) ? "active" : "";
+          html += '<li class=\"'+active+' page-item">\
+            <a class="page-link" data-value=\"'+index+'"\ >'+index+'</a></li>'
+
+          if(index == pageCount) {
+            break;
+          }
+        }
+        if((pRCnt + 1) * pageSize < pageCount) {
+          html += '<li class="page-item">\
+                <a class="page-link" data-value=\"'+((pRCnt + 1)*pageSize+1)+'"\ aria-label="Next">\
+                  <i class="ti-angle-right f-12"></i>\
+                  <span aria-hidden="true"></span>\
+                  <span class="sr-only">Next</span>\
+                </a></li>\
+                <li class="page-item">\
+                <a class="page-link" data-value=\"'+pageCount+'"\ aria-label="Next">\
+                  <i class="ti-angle-double-right f-12"></i>\
+                  <span aria-hidden="true"></span>\
+                  <span class="sr-only">Next</span>\
+                </a></li>';
+        }
+        html += '</ul></td></tr>';
+        $('#listTable tfoot').append(html);
+      }
+      $('.preloader3').fadeOut(500);
+    }
+  });
+}
+// 리스트 조건 세팅
+function startSetting(){
+  var mcp = $.urlParam("mcp");
+  if(mcp){
+    $('#selectMCP > option[value='+mcp+']').attr("selected",true);
+  }
+  var cp = $.urlParam("cp");
+  if(cp){
+    $('#selectCP > option[value='+cp+']').attr("selected",true);
+  }
+  var searchType = $.urlParam("searchType");
+  if(searchType){
+    $('#selectSearchType > option[value='+searchType+']').attr("selected",true);
+  }
+  var search = $.urlParam("search");
+  if(search){
+    $('#searchInput').val(search);
+  }
+  var sDate = $.urlParam("sDate");
+  var eDate = $.urlParam("eDate");
+  console.log(sDate,eDate);
+  $('#reportrange').daterangepicker(optionSet1,cb);
+  if((sDate != '' && eDate != '') && (sDate != null && eDate != null)){
+    $('#reportrange span').html(moment(new Date(sDate)).format('YYYY.MM.DD') + ' - ' + moment(new Date(eDate)).format('YYYY.MM.DD'));
+  }
+}
+// 리스트 조건 param 세팅
+function settingParams(num){
+  var renewURL = window.location.origin+window.location.pathname;
+  var param = {};
+  if(Number.isInteger(num)){
+    param.page = num;
+    if(typeof history.pushState == 'function'){
+      renewURL += '?page='+param.page;
+      history.pushState(null, null,renewURL);
+    }
+  }
+  // MCP
+  var mcpValue = $('#selectMCP option:selected').val();
+  if(mcpValue != ''){
+    param.mcp = mcpValue;
+    if(typeof history.pushState == 'function' && Number.isInteger(num)){
+      renewURL += '&mcp='+param.mcp;
+      history.pushState(null, null,renewURL);
+    }
+  }
+  // CP
+  var cpValue = $('#selectCP option:selected').val();
+  if(cpValue != ''){
+    param.cp = cpValue;
+    if(typeof history.pushState == 'function' && Number.isInteger(num)){
+      renewURL += '&cp='+param.cp;
+      history.pushState(null, null,renewURL);
+    }
+  }
+  //검색
+  var searchValue = $('#searchInput').val();
+  if(searchValue != ''){
+    param.searchType = $('#selectSearchType option:selected').val();
+    param.search = searchValue;
+    if(typeof history.pushState == 'function' && Number.isInteger(num)){
+      renewURL += '&search='+encodeURI(param.search);
+      renewURL += '&searchType='+param.searchType;
+      history.pushState(null, null,renewURL);
+    }
+  }
+  //날짜검색
+  var dateArr = $("#reportrange span").text().split(' - ');
+  if(dateArr.length > 1){
+    var start = moment(new Date(dateArr[0]));
+    var end = moment(new Date(dateArr[1]));
+    param.sDate = start.format('YYYY-MM-DD');
+    param.eDate = end.format('YYYY-MM-DD');
+    if(typeof history.pushState == 'function' && Number.isInteger(num)){
+      renewURL += '&sDate='+param.sDate;
+      renewURL += '&eDate='+param.eDate;
+      history.pushState(null, null,renewURL);
+    }
+  }
+  return param;
+}
+// 검색 이벤트
+function searchFun(){
+  var param = settingParams(1);
+  ajaxGetPageList(param);
+}
+//날짜 설정
+var cb = function(start, end, label) {
+  console.log('FUNC: cb',start.toISOString(), end.toISOString(), label);
+  $('#reportrange span').html(moment(new Date(start.toISOString())).format('YYYY.MM.DD') + ' - ' + moment(new Date(end.toISOString())).format('YYYY.MM.DD'));
+  searchFun();
+}
+var ranges = {
+  '당일': [moment(), moment()],
+  '전일': [moment().subtract(1, 'days'), moment()],
+  '최근 7일': [moment().subtract(6, 'days'), moment()],
+  '최근 30일': [moment().subtract(29, 'days'), moment()]
+};
+var sDateParams = $.urlParam("sDate");
+var eDateParams = $.urlParam("eDate");
+var optionSet1 = {
+  startDate: (sDateParams != '' && sDateParams != null) ? moment(new Date(sDateParams)) : moment(),
+  endDate:  (eDateParams != '' && eDateParams != null) ? moment(new Date(eDateParams)) : moment(),
+  showDropdowns: true,
+  showRangeInputsOnCustomRangeOnly: false,
+  ranges: ranges,
+  opens: 'left',
+  buttonClasses: ['btn'],
+  applyClass: 'btn-small btn-primary',
+  cancelClass: 'btn-small',
+  format: 'YYYY.MM.DD',
+  separator: ' to ',
+  locale: {
+    applyLabel: '확인',
+    cancelLabel: '취소',
+    customRangeLabel: '직접선택',
+    daysOfWeek: ['일', '월', '화', '수', '목', '금', '토'],
+    monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+    firstDay: 1
+  }
+};
--- autogreen/routes/mcp/monitoring.js
+++ autogreen/routes/mcp/monitoring.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var xl = require('excel4node');
+var moment = require('moment');
+var datetime = require('node-datetime');
+var fs = require('fs');
 var router = express.Router();
-// DB module
+// DB modulevar fs = require('fs');
 var contents = require('../../models/mcp/contents.js');
 var monitoring = require('../../models/mcp/monitoring.js');
 
@@ -19,6 +23,175 @@ router.get('/:type',isAuthenticated,async function(req, res, next) {
 router.post('/:type/getNextPage',isAuthenticated,async function(req, res, next) {
   var data = await getListPageData(req.params.type,req.body);
   res.send({status:true,result:data});
+});
+
+const aDir = 'C:/gitProject/webhard/autogreen/';
+router.get('/:type/excel',async function(req, res) {
+  var typeVal = (req.params.type == 'alliance')? '1':'0';
+  var list = await monitoring.selectView(req.query,[typeVal]);
+  var wb = new xl.Workbook({
+    defaultFont: {
+      size: 12,
+      color: '#000000'
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#FFFFFF'
+    },
+    alignment: {
+      horizontal: 'left'
+    },
+    numberFormat: '$#,##0;-'
+  });
+  var ws = wb.addWorksheet('sheet1');
+  var tStyle = wb.createStyle({
+    font: {
+      bold: true
+    },
+    fill: {
+      type: 'pattern',
+      patternType: 'solid',
+      fgColor: '#ffeb3b',
+    },
+    alignment: {
+      horizontal: 'center'
+    }
+  });
+  // ws.column(3).setWidth(60);
+  ws.cell(1,1).string('NO').style(tStyle);
+  ws.cell(1,2).string('OSP').style(tStyle);
+  ws.cell(1,3).string('MCP').style(tStyle);
+  ws.cell(1,4).string('CP').style(tStyle);
+  ws.cell(1,5).string('콘텐츠제목').style(tStyle);
+  ws.cell(1,6).string('키워드').style(tStyle);
+  ws.cell(1,7).string('제목').style(tStyle);
+  ws.cell(1,8).string('게시물번호').style(tStyle);
+  ws.cell(1,9).string('판매금액').style(tStyle);
+  ws.cell(1,10).string('판매자').style(tStyle);
+  ws.cell(1,11).string('검출일').style(tStyle);
+  ws.cell(1,12).string('채증시간').style(tStyle);
+  ws.cell(1,13).string('URL').style(tStyle);
+  ws.cell(1,14).string('채증이미지').style(tStyle);
+  var row = 0;
+  await asyncForEach(list, async (item, index, array) => {
+    row = index+2;
+    var dateStr = (item.go_regdate == null) ? '':item.go_regdate;
+    var dateArr = dateStr.split(',');
+    if(dateArr.length == 3){
+      dateStr = '1차 :'+dateArr[0]+', 2차 :'+dateArr[1]+', 3차 :'+dateArr[2];
+    }
+    else if(dateArr.length == 2){
+      dateStr = '1차 :'+dateArr[0]+', 2차 :'+dateArr[1]
+    }
+    else if(dateArr.length == 1){
+      dateStr = '1차 :'+dateArr[0]
+    }
+    ws.cell(row,1).string((index+1).toString());
+    ws.cell(row,2).string(item.osp_sname);
+    ws.cell(row,3).string(item.cnt_mcp);
+    ws.cell(row,4).string(item.cnt_cp);
+    ws.cell(row,5).string(item.cnt_title);
+    ws.cell(row,6).string(item.k_title);
+    ws.cell(row,7).string(item.title);
+    ws.cell(row,8).string(item.cnt_num);
+    ws.cell(row,9).string((item.cnt_price == null) ? '':item.cnt_price);
+    ws.cell(row,10).string((item.cnt_writer == null) ? '':item.cnt_writer);
+    ws.cell(row,11).string(dateStr);
+    ws.cell(row,12).string('');
+    ws.cell(row,13).string(item.cnt_url);
+    ws.cell(row,14).string(item.cnt_img_name);
+  });
+
+  var date = datetime.create();
+  var today = date.format('YmdHM');
+  var filename = 'autogreen_result_'+today+'.xlsx';
+  var filepath = aDir+filename;
+  wb.write(filepath,function(err,stats){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.setHeader("Content-Type", "application/x-msdownload");
+      res.setHeader("Content-Disposition", "attachment; filename=" + filename);
+
+      var filestream = fs.createReadStream(filepath);
+      fs.unlink(filepath,function(err){
+        if(err) return console.log(err);
+        console.log('file deleted successfully');
+      });
+      filestream.pipe(res);
+    }
+  });
+});
+
+router.get('/:type/image',async function(req, res) {
+  var zip = new require('node-zip')();
+  var typeVal = (req.params.type == 'alliance')? '1':'0';
+  var list = await monitoring.selectImage(req.query,[typeVal]);
+  var count = 0;
+  await asyncForEach(list, async (item, index, array) => {
+    try {
+      zip.file(item.cnt_img_name, fs.readFileSync(aDir+'/public/monitoring_img'+item.path+'/'+item.cnt_img_name));
+      count += 1;
+    } catch (e) {
+        console.log('ERROR : ',e);
+    }
+  });
+  if(count == 0){
+    res.redirect('/monitoring/'+req.params.type+'?z-result=false');
+    return false;
+  }
+  var date = datetime.create();
+  var today = date.format('YmdHM');
+  var filename = 'autogreen_images_'+today+'.zip';
+  var filepath = aDir+filename;
+  var data = zip.generate({base64:false, compression:'DEFLATE'});
+  fs.writeFileSync(filename, data, 'binary');
+  res.setHeader("Content-Type", "application/x-msdownload");
+  res.setHeader("Content-Disposition", "attachment; filename=" + filename);
+  var filestream = fs.createReadStream(filepath);
+  fs.unlink(filepath,function(err){
+    if(err) return console.log(err);
+    console.log('zip file deleted successfully');
+  });
+  filestream.pipe(res);
+
+});
+
+router.post('/delete',isAuthenticated,async function(req, res, next) {
+  // 키워드 필터링 리스트
+  var result = await monitoring.delete('cnt_f_list',{'n_idx':req.body.idx});
+  console.log(result);
+  if(!('protocol41' in result)){
+    res.status(500);
+    return false;
+  }
+  // 키워드 필터링 상세 리스트
+  result = await monitoring.delete('cnt_f_detail',{'f_idx':req.body.idx});
+  if(!('protocol41' in result)){
+    res.status(500);
+    return false;
+  }
+  // 2, 3차 모니터링 리스트
+  result = await monitoring.delete('go_site',{'cnt_url':req.body.url});
+  if(!('protocol41' in result)){
+    res.status(500);
+    return false;
+  }
+  // 이미지 채증 리스트
+  result = await monitoring.delete('go_img',{'cnt_url':req.body.url});
+  if(!('protocol41' in result)){
+    res.status(500);
+    return false;
+  }
+  res.send(true);
+});
+
+router.post('/getInfo',isAuthenticated,async function(req, res, next) {
+  // 키워드 필터링 리스트
+  var result = await monitoring.getInfo(req.body.idx);
+  res.send(result);
 });
 
 async function getListPageData(type,param){
@@ -78,6 +251,15 @@ async function getListPageData(type,param){
     console.log(e);
   }
   return data;
+}
+
+async function asyncForEach(array, callback) {
+  for (var index = 0; index < array.length; index++) {
+   var done = await callback(array[index], index, array);
+    if(done == false){
+     break;
+    }
+  }
 }
 
 module.exports = router;
