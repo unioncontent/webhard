e4d38783a74310101d1def1d8dc9391d69955ea6
DESKTOP-RC17VBI\user <noraplay@daum.net>
2018-11-14 10:57:44 +0900

    경로오류수정

--- autogreen/routes/mcp/setting.js
+++ autogreen/routes/mcp/setting.js
@@ -301,7 +301,7 @@ router.post('/file_upload',async function (req, res) {
 router.post('/file_delete',async function (req, res) {
   // fs.unlinkSync(filePath);
   // res.send(true);
-  fs.unlink('C:/gitProject/webhard/autogreen/'+req.body.path, function (err) {
+  fs.unlink('C:/Users/user/Documents/webhard/autogreen/'+req.body.path, function (err) {
     if (err){
       console.log(err);
     }
