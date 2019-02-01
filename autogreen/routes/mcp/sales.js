8f7284eae14143bba171ea0e08d6e9e62a1ac2a9
BYUN SOOYEON <smb1457@naver.com>
2019-02-01 11:00:12 +0900

    mcp 매출통계 개발중

--- autogreen/routes/mcp/sales.js
+++ autogreen/routes/mcp/sales.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 // DB module
 var osp = require('../../models/mcp/osp.js');
 var sales = require('../../models/mcp/sales.js');
@@ -13,12 +14,75 @@ var isAuthenticated = function (req, res, next) {
 };
 
 router.get('/',isAuthenticated,async function(req, res, next) {
-  // var data = await getListPageData(req.query,req.user);
-  var data = {};
+  var data = await getListPageData(req.query,req.user);
+  console.log(data);
   data.ospList = await osp.selectOSPList_m(2);
   data.mcpList = await contents.getMCPList('m');
   res.render('mcp/sales',data);
 });
+
+router.post('/getNextPage',isAuthenticated,async function(req, res, next) {
+  var data = await getListPageData(req.body,req.user);
+  res.send({status:true,result:data});
+});
+
+async function getListPageData(param,user){
+  var sDate = new Date();
+  var eDate = new Date();
+  sDate.setMonth(sDate.getMonth()-5);
+  var start = sDate.getFullYear()+'-'+((sDate.getMonth()+1 < 10) ? '0'+(sDate.getMonth()+1):sDate.getMonth()+1);
+  var end = eDate.getFullYear()+'-'+((eDate.getMonth()+1 < 10) ? '0'+(eDate.getMonth()+1):eDate.getMonth()+1);
+  var data = {
+    graph:[],
+    list:[],
+    cp:'0',
+    mcp:'0',
+    osp:'0',
+    sDate:start,
+    eDate:end
+  };
+  var searchParam = [data.mcp,data.cp,data.osp,data.sDate,data.eDate];
+  if (typeof param.mcp !== 'undefined') {
+    searchParam[0] = param.mcp;
+    data['mcp'] = param.mcp;
+  }
+  // else if(data['mcp'] != '') {
+  //   searchParam[0] = data['mcp'];
+  // }
+
+  if (typeof param.cp !== 'undefined') {
+    searchParam[1] = param.cp;
+    data['cp'] = param.cp;
+  }
+  // else if(data['cp'] != '') {
+  //   searchParam[1] = data['cp'];
+  // }
+
+  if (typeof param.osp !== 'undefined') {
+    searchParam[2] = param.osp;
+    data['osp'] = param.osp;
+  }
+  if (typeof param.sDate !== 'undefined') {
+    if (typeof param.eDate !== 'undefined') {
+      searchParam[3] = param.sDate;
+      searchParam[4] = param.eDate;
+      data['sDate'] = param.sDate;
+      data['eDate'] = param.eDate;
+    }
+  }
+
+  try{
+    // call_sales
+    var result = await sales.call_sales(searchParam);
+    data['graph'] = JSON.stringify(result[0]);
+    data['list'] = ((result[1].length == 0)?[]:result[1]);
+    data['totalResult'] = result[2][0];
+  }
+  catch(e){
+    console.log(e);
+  }
+  return data;
+}
 
 router.get('/add',isAuthenticated,async function(req, res, next) {
   var data = {};
@@ -52,4 +116,5 @@ router.post('/add/search',isAuthenticated,async function(req, res, next) {
   var cntList = await sales.selectCnt(req.body.search);
   res.send({list:cntList,status: true});
 });
+
 module.exports = router;
