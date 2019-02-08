d5b2ec04ae6fcb944856d80a3b4deb885ac239ec
BYUN SOOYEON <smb1457@naver.com>
2019-02-08 17:36:50 +0900

    otogreen 매출통계 페이지 구현(TEST)

--- autogreen/models/mcp/sales.js
+++ autogreen/models/mcp/sales.js
@@ -6,6 +6,46 @@ var sales = {
     var sql = "call site.sales(?,?,?,?,?)";
     return await getResult(sql,param);
   },
+  selectDetail: async function(param){
+    var whereQuery = "where s_osp = '"+param['osp']+"'";
+    if('cp' in param){
+      whereQuery += ' and s_cp = \''+param['cp']+'\'';
+    }
+    if('mcp' in param){
+      whereQuery += ' and s_mcp = \''+param['mcp']+'\'';
+    }
+    if(('sDate' in param) && ('eDate' in param)){
+      whereQuery+=" and DATE_FORMAT(STR_TO_DATE(s_settlement_date,'%Y-%m'), '%Y-%m') between '"+param['sDate']+"' and '"+param['eDate']+"' ";
+    }
+    var sql = "select n_idx, s_mcp, s_cp, s_osp, s_cnt_num, s_title,DATE_FORMAT(STR_TO_DATE(s_settlement_date,'%Y-%m'), '%Y-%m') as s_settlement_date, s_regdate, format(sum(s_total_money),0) as s_total_money, format(sum(s_total_sales),0) as s_total_sales, format(sum(s_settlement_money),0) as s_settlement_money from sales "+whereQuery+" group by STR_TO_DATE(s_settlement_date,'%Y-%m') order by STR_TO_DATE(s_settlement_date,'%Y-%m') desc";
+    if(!('type' in param)){
+      sql += " limit ?,?";
+      return await getResult(sql,[param['limit'],param['offset']]);
+    }
+    else{
+      return await getResult(sql);
+    }
+  },
+  selectDetailCount: async function(param){
+    var whereQuery = "where s_osp = '"+param['osp']+"'";
+    if('cp' in param){
+      whereQuery += ' and s_cp = \''+param['cp']+'\'';
+    }
+    if('mcp' in param){
+      whereQuery += ' and s_mcp = \''+param['mcp']+'\'';
+    }
+    if(('sDate' in param) && ('eDate' in param)){
+      whereQuery+=" and DATE_FORMAT(STR_TO_DATE(s_settlement_date,'%Y-%m'), '%Y-%m') between '"+param['sDate']+"' and '"+param['eDate']+"' ";
+    }
+    var sql = "select count(*) as total from (select * from sales "+whereQuery+" group by STR_TO_DATE(s_settlement_date,'%Y-%m') order by STR_TO_DATE(s_settlement_date,'%Y-%m') desc) a";
+    var count = await getResult(sql);
+    if(count.length == 0){
+      return 0;
+    }
+    else{
+      return count[0]['total'];
+    }
+  },
   selectCnt: async function(param){
     var sql = "select * from cnt_l_list where cnt_id  like '%"+param+"%' or replace(cnt_title,' ','') like '%"+param+"%'";
     return await getResult(sql);
